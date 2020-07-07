7d2a9e5b6c050da62435b01f3c69a0aad9a11bed
KonstErz <konstantin.yerzin@gmail.com>
2020-07-07 15:48:43 +0700

    init.sh file changed

--- init.sh
+++ init.sh
@@ -4,7 +4,3 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
-
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database s_web_project;"
-mysql -uroot -e "grant all privileges on s_web_project.* to 'box'@'localhost' with grant option;"
