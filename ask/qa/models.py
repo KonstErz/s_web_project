6c606e8d4afa25445cd7fc4e1b7135d09e5f5ead
KonstErz <konstantin.yerzin@gmail.com>
2020-07-11 20:42:23 +0700

    views changed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,38 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
 class QuestionManager(models.Manager):
-
     def new(self):
-        return self.order_by('-added_at')
+        return self.order_by('-id')
 
     def popular(self):
         return self.order_by('-rating')
 
 
 class Question(models.Model):
-
     objects = QuestionManager()
-
-    title = models.CharField(max_length=255)
-    text = models.TextField()
+    title = models.CharField(default='', max_length=255)
+    text = models.TextField(default='')
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
-    def get_absolute_url(self):
-        return reverse('question-detail', args=[str(self.id)])
+    def get_url(self):
+        return f'/question/{self.id}'
 
     def __str__(self):
         return self.title
 
 
 class Answer(models.Model):
-
-    text = models.TextField()
+    text = models.TextField(default='')
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
