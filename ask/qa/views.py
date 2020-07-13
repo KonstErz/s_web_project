59d1b50d843afe8c99ae1afe3a4ee2b9f48c14e4
KonstErz <konstantin.yerzin@gmail.com>
2020-07-13 16:51:01 +0700

    models and views changed (3)

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
 from .models import Question, Answer
 
 
@@ -8,41 +9,57 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
-def question_list(request):
-    qs = Question.objects.all().order_by('-id')
-    limit = request.GET.get('limit', 10)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(qs, limit)
-    page = paginator.page(page)
-    return render(request, 'question_list.html',
-                  {'title': 'New',
-                   'questions': page.object_list,
-                   'paginator': paginator,
-                   'page': page, })
-
-
-def popular(request):
-    qs = Question.objects.all().order_by('-rating')
-    limit = request.GET.get('limit', 10)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(qs, limit)
-    page = paginator.page(page)
-    return render(request, 'question_list.html',
-                  {'title': 'Popular',
-                   'questions': page.object_list,
-                   'paginator': paginator,
-                   'page': page, })
-
-
-def question_details(request, num):
+def paginate(request, qs):
     try:
-        question = Question.objects.get(id=num)
-    except Question.DoesNotExist:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
         raise Http404
+    paginator = Paginator(qs, limit)
     try:
-        answers = Answer.objects.filter(question=question)
-    except Answer.DoesNotExist:
-        answers = None
-    return render(request, 'question_detail.html',
-                  {'question': question,
-                   'answers': answers, })
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return paginator, page, limit
+
+
+@require_GET
+def question_list(request, *args, **kwargs):
+    questions = Question.objects.order_by('-id')
+    paginator, page, limit = paginate(request, questions)
+    context = {
+        'title': 'List of New Questions',
+        'questions': page,
+        'paginator': paginator,
+        'limit': limit,
+    }
+    return render(request, 'question_list.html', context)
+
+
+@require_GET
+def popular(request, *args, **kwargs):
+    questions = Question.objects.popular()
+    paginator, page, limit = paginate(request, questions)
+    context = {
+        'title': 'List of Popular Questions',
+        'questions': page,
+        'paginator': paginator,
+        'limit': limit,
+    }
+    return render(request, 'question_list.html', context)
+
+
+def question_details(request, pk):
+    question = get_object_or_404(Question, id=pk)
+    answers = Answer.objects.filter(question=pk).order_by('-added_at')
+    context = {
+        'title': 'Question Page',
+        'question': question,
+        'answers': answers,
+    }
+    return render(request, 'question_detail.html', context)
