b8b2aa87451a3ee4b9056d7a48e1bc5c2a6218a1
KonstErz <konstantin.yerzin@gmail.com>
2020-07-29 16:41:19 +0700

    files with commands and requirements created

---
+++ commands_2.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install python3-dev
+sudo apt install mysql-server mysql-client
+sudo apt install libmysqlclient-dev build-essential
+pip install --upgrade pip
+pip install -r requirements.txt
